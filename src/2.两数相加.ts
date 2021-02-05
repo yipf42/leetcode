bbbcdfc295040e9eed45e90577d4a763eb33519b
yipengfei <yipengfei@meituan.com>
2021-02-05 16:27:11 +0800

    feat: 1,2,3,4

---
+++ src/2.两数相加.ts
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=typescript
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null, carryOver: number = 0): ListNode | null {
+  if (l1 === null && l2 === null) {
+    return (carryOver === 0) ? null : new ListNode(carryOver);
+  }
+
+  const currentSum = (l1?.val || 0) + (l2?.val || 0) + carryOver;
+  return new ListNode(
+    currentSum % 10,
+    addTwoNumbers(
+      l1?.next || null,
+      l2?.next || null,
+      (currentSum / 10) >> 0
+    )
+  );
+}
+// @lc code=end
+
---
+++ src/4.寻找两个正序数组的中位数.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=typescript
+ *
+ * [4] 寻找两个正序数组的中位数
+ */
+
+// @lc code=start
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+  const array = nums1.concat(nums2).sort((a, b) => a - b);
+  const length = array.length;
+  if (length > 0 && length % 2 === 0) {
+    return (array[length / 2 - 1] + array[length / 2]) / 2;
+  } else {
+    return array[(length - 1) / 2];
+  }
+};
+// @lc code=end
+
